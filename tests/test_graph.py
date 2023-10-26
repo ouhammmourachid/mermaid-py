0b1a6634ebbd42404e151097553f0aad65f23e6d
ouhammmourachid <rachidouhammou21@gmail.com>
2023-10-26 22:55:06 +0100

    add test for name that contain space

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -23,9 +23,16 @@ class TestGraph(unittest.TestCase):
         self.graph_test.save(Path('./test-graph-file.txt'))
         self.assertTrue(Path.exists(Path('./test-graph-file.txt')))
 
+    def test_save_graph_name_contain_space(self) -> None:
+        self.graph_test.name = 'test graph file'
+        self.graph_test.save(Path('./test graph file.txt'))
+        self.assertTrue(Path.exists(Path('./test graph file.txt')))
+
     def tearDown(self) -> None:
         if os.path.exists('./test-graph.txt'):
             os.remove('./test-graph.txt')
         if os.path.exists('./test-graph-file.txt'):
             os.remove('./test-graph-file.txt')
+        if os.path.exists('./test graph file.txt'):
+            os.remove('./test graph file.txt')
         return super().tearDown()
