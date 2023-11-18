cb6d8bd353de016442c3737b73b9f7b58f620341
ouhammmourachid <rachidouhammou21@gmail.com>
2023-11-18 14:53:36 +0100

    add docstring to userjourney module

--- mermaid/userjourney/section.py
+++ mermaid/userjourney/section.py
@@ -2,11 +2,31 @@ from .task import Task
 
 
 class Section:
+    """Section class.
+
+    This class represents a section in a user's journey.
+    Each section contains a list of tasks.
+
+    Attributes:
+        name (str): The name of the section.
+        tasks (list[Task]): The tasks in the section.
+    """
     def __init__(self, name: str, tasks: list[Task]) -> None:
+        """Initialize a new Section.
+
+        Args:
+            name (str): The name of the section.
+            tasks (list[Task]): The tasks in the section.
+        """
         self.name: str = name
         self.tasks: list[Task] = tasks
 
     def __str__(self) -> str:
+        """Return a string representation of the section.
+
+        Returns:
+            str: A string representation of the section.
+        """
         string: str = f'\tsection {self.name}\n'
         for task in self.tasks:
             string += f'{task}\n'
--- mermaid/userjourney/task.py
+++ mermaid/userjourney/task.py
@@ -4,13 +4,30 @@ from .actor import Actor
 
 
 class Task:
+    """Task class.
+
+    This class represents a task in a section of a user's journey.
+
+    Attributes:
+        name (str): The name of the task.
+    """
     def __init__(self, name: str, score: int, actors: Union[list[Actor],
                                                             Actor]) -> None:
+        """Initialize a new Task.
+
+        Args:
+            name (str): The name of the task.
+        """
         self.name: str = name
         self.score: int = score
         self.actors: list[Actor] = actors if isinstance(actors,
                                                         list) else [actors]
 
     def __str__(self) -> str:
+        """Return a string representation of the task.
+
+        Returns:
+            str: A string representation of the task.
+        """
         actor_string: str = ', '.join([actor.name for actor in self.actors])
         return f'\t\t{self.name}: {self.score} : {actor_string}'
