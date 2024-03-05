a03afbbcdcc0c3bde8a22282f4c91ca743d9535d
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-03-05 13:31:35 +0100

    Update theme.config.tsx

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>mermaid-py</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ouhammmourachid/mermaid-py',
   },
   chat: {
     link: 'https://discord.com',
