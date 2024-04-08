8a692bf1fa17bcb8d85696433202c1fe61c26421
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-04-08 18:01:17 +0000

    Add unit tests for the Icon class in test_icon.py

---
+++ mermaid/tests/test_icon.py
@@ -0,0 +1,12 @@
+import unittest
+from mermaid.icon import Icon
+
+
+class TestIcon(unittest.TestCase):
+    def test_str_icon_v1(self):
+        icon = Icon(name='database', type_='fa')
+        self.assertEqual('fa database', str(icon))
+    
+    def test_str_icon_v2(self):
+        icon = Icon(name='database', type_='fa', str_='v2')
+        self.assertEqual(' fa:database ', str(icon))
