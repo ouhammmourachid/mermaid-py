1bfddbd368aabb59de5a336a329a2a13e16541db
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-07-20 10:47:32 +0000

    refactor: Update Icon class to use version instead of str_

---
+++ tests/test_icon.py
@@ -0,0 +1,16 @@
+import unittest
+
+from mermaid.icon import Icon
+
+
+
+class TestIcon(unittest.TestCase):
+    def setUp(self) -> None:
+        self.icon_v1 = Icon('icon', 'fa')
+        self.icon_v2 = Icon('icon', 'fa', version='v2')
+        self.icon_v3 = Icon('icon', 'fa', version='v3')
+    
+    def test_str(self):
+        self.assertEqual(str(self.icon_v1), 'fa icon')
+        self.assertEqual(str(self.icon_v2), 'fa:icon')
+        self.assertEqual(str(self.icon_v3), ' fa:icon ')
