59a07193c1f8096cbf9fb29765403a01b6f09cea
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-07-21 12:32:42 +0100

    Refactor module imports and file structure (#163)
    * Refactor module imports and file structure
    
    * chore: Update pyproject.toml dependencies
    
    This commit updates the dependencies in the pyproject.toml file. The `ipython` dependency has been removed.
    
    Note: This message does not include any issue references, tags, or author names.

--- mermaid/graph.py
+++ mermaid/graph.py
@@ -8,7 +8,7 @@ Classes:
 """
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
 from mermaid.configuration import Config
 
@@ -29,20 +29,25 @@ class Graph:
     script: str
     config: Optional[Config] = None
 
-    def save(self, path: Optional[Path] = None) -> None:
+    def save(self, path: Optional[Union[Path, str]] = None) -> None:
         """Save the diagram to a file.
 
         Args:
-            path (Optional[Path]): The path to save the diagram. If not
+            path (Optional[Union[Path,str]]): The path to save the diagram. If not
                 provided, the diagram will be saved in the current directory
                 with the title as the filename.
         Raises:
             ValueError: If the file extension is not '.mmd' or '.mermaid'.
         """
-        file_path: Path = path if path else Path(f'./{self.title}.mmd')
-        if file_path.suffix not in ['.mmd', '.mermaid']:
+
+        if path is None:
+            path = Path(f'./{self.title}.mmd')
+        if isinstance(path, str):
+            path = Path(path)
+
+        if path.suffix not in ['.mmd', '.mermaid']:
             raise ValueError("File extension must be '.mmd' or '.mermaid'")
-        with open(file_path, 'w') as file:
+        with open(path, 'w') as file:
             file.write(self.script)
 
     def _build_script(self) -> None:
